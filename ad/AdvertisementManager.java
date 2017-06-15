434d549f6f0b353f45c7ba917fc04e9dee111262
Alexey Malakhov <au.malakhov@gmail.com>
2017-06-15 14:17:34 +0300

    Restourant step9

--- ad/AdvertisementManager.java
+++ ad/AdvertisementManager.java
@@ -1,8 +1,20 @@
 package com.javarush.task.task27.task2712.ad;
 
+import com.javarush.task.task27.task2712.ConsoleHelper;
+
 /**
  * Created by amalakhov on 15.06.2017.
  */
 public class AdvertisementManager {
     private final AdvertisementStorage storage = AdvertisementStorage.getInstance();
+    private int timeSeconds;
+
+    public AdvertisementManager(int timeSeconds) {
+        this.timeSeconds = timeSeconds;
+    }
+
+    public void processVideos(){
+        ConsoleHelper.writeMessage("calling processVideos method");
+    }
+
 }
